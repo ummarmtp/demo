a1323582196d3bff8fa936da938d2fd4fec8dad1
ummarmtp <ummarmtp@gmail.com>
2025-03-13 13:38:58 +0400

    "change"

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     const clientIp = req.socket.remoteAddress;
     console.log(`Client IP: ${clientIp}`);
-     
+    
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Your IP has been logged on the server console.');
 });
 
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running at http://<your-server-ip>:${PORT}/`);
+app.get('/', (req, res) => {
+    res.send(`Client IP: ${req.ip}`);
 });
